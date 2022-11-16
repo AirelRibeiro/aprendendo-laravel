1eabfbf72266fa34808e86d98c02480fd4b7de4c
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-16 13:38:53 -0300

    feat: inclui view para rota get words
    -- Views construídas usando templates Blade,
    Na documentação: https://laravel.com/docs/9.x/blade#introduction

---
+++ app/resources/views/words/index.blade.php
@@ -0,0 +1,37 @@
+@extends('app')
+
+@section('title', 'Palavras')
+
+@section('content')
+<h1>Lista de Palavras</h1>
+<table class="table table-hover">
+  <thead class="table-dark">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Palavra</th>
+      <th scope="col">Significado</th>
+      <th scope="col">Opções</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($words as $word)
+    <tr>
+      <th scope="row">{{ $word->id }}</th>
+      <td>{{ $word->word }}</td>
+      <td>{{ $word->meaning }}</td>
+      <td>
+        <a class="btn btn-primary" href="#">Ver detalhes</a>
+        <form action="#" method="POST" style="display: inline;">
+          @method('DELETE')
+          @csrf
+          <button class="btn btn-danger" type="submit" onclick="return confirm('Tem certeza que deseja apagar?')">Apagar</button>
+        </form>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+<div class="d-grid gap-2 col-6 mx-auto">
+  <a class="btn btn-success" href="#">Incluir nova palavra</a>
+</div>
+@endsection
