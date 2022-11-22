9eea42a84dd10271609ec2e3e028208848ff36b6
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-22 17:59:36 -0300

    feat: inclui proteção de rotas
    -- Middleware auth sendo usado para que apenas users logados possam acessar rotas "principais" da aplicação

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', HomeController::class)->name('home');
+    Route::resource('words', WordsController::class);
+});
+
 Route::get('/', function () {
     return view('app');
 });
