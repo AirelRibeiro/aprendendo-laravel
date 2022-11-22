75ce49f7e52338272d0f24b63ae9e3f2b44b408a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-22 17:34:27 -0300

    feat: inclui RegisterController
    -- Controller para possibilitar inclusão de users no banco, possui dois métodos:
      -- create: que chama a view register, com o form de registro;
      -- store: que valida os dados e chama o model para salvar a pessoa usuária

---
+++ app/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'min:10', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+
+        $dados = $request->only(['name', 'email', 'password']);
+        $dados['password'] = Hash::make($dados['password']);
+
+        User::create($dados);
+
+        return redirect()->route('home');
+    }
+}
